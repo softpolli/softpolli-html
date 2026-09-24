f482477094e8bfeb8c95e7b01cae20e4d4e8340c
Sabbir Ahmed <sabbirbd912@gmail.com>
2026-09-25 00:21:46 +0600

    client section color changed

--- assets/hero/hero.js
+++ assets/hero/hero.js
@@ -1,17 +1,17 @@
- const softpolliSwiper = new Swiper(".softpolliSwiper", {
-    direction: "vertical",
-    loop: false,              // <-- loop off
-    speed: 1200,
-    effect: "fade",
-    fadeEffect: { crossFade: true },
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-      pauseOnMouseEnter: false,
-    },
-    pagination: {
-      el: ".softpolliSwiper .swiper-pagination",
-      clickable: true,
-    },
-    navigation: { nextEl: ".soft-next", prevEl: ".soft-prev" },
-  });
+const softpolliSwiper = new Swiper(".softpolliSwiper", {
+  direction: "vertical",
+  loop: false,              // <-- loop off
+  speed: 1200,
+  effect: "fade",
+  fadeEffect: { crossFade: true },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: false,
+  },
+  pagination: {
+    el: ".softpolliSwiper .swiper-pagination",
+    clickable: true,
+  },
+  navigation: { nextEl: ".soft-next", prevEl: ".soft-prev" },
+});
